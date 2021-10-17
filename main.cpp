e0c40f50bdc1838bde2a74e2a65471cb342c9839
Pengalu <pengalu99@gmail.com>
2021-10-17 00:22:15 -0700

    ahhh

--- main.cpp
+++ main.cpp
@@ -4,24 +4,9 @@
 using namespace std;
 
 
-#include <libraryheaders>
 
 int main()
 {
     cout << "Hello World" << endl;
-    createWindow(title, width, height);
-    createOpenGLContext(settings);
 
-    while (windowOpen)
-    {
-        while (event = newEvent())
-            handleEvent(event);
-
-        updateScene();
-
-        drawGraphics();
-        presentGraphics();
-    }
-
-    return 0;
 }
